fe07ae99d05f9a3208340eacdff0ded18d431a00
manager <credongoukeng.aws@gmail.com>
2023-11-03 12:49:48 +0100

    new sytles

--- src/Komponenten/NotizFormular/NotizBearbeitenFormular.js
+++ src/Komponenten/NotizFormular/NotizBearbeitenFormular.js
@@ -1,16 +1,41 @@
-// src/Komponenten/NotizFormular/NotizBearbeitenFormular.js
-import React from 'react';
+import React, { useState } from 'react';
 
-class NotizBearbeitenFormular extends React.Component {
-  // Logik für das Bearbeiten einer vorhandenen Notiz hier implementieren
-  render() {
-    return (
-      <div>
-        {/* Formularelemente hier */}
-        <p>Notiz bearbeiten</p>
-      </div>
-    );
-  }
-}
+const NotizBearbeitenFormular = ({ notiz, aktualisierenNotiz }) => {
+  const [titel, setTitel] = useState(notiz.titel);
+  const [inhalt, setInhalt] = useState(notiz.inhalt);
+
+  const handleTitelChange = (e) => {
+    setTitel(e.target.value);
+  };
+
+  const handleInhaltChange = (e) => {
+    setInhalt(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Hier kannst du die Funktion aktualisierenNotiz aufrufen und die aktualisierten Notizdaten übergeben
+    aktualisierenNotiz(notiz.id, { titel, inhalt });
+  };
+
+  return (
+    <div>
+      <h2>Notiz bearbeiten</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Titel:
+          <input type="text" value={titel} onChange={handleTitelChange} />
+        </label>
+        <br />
+        <label>
+          Inhalt:
+          <textarea value={inhalt} onChange={handleInhaltChange} />
+        </label>
+        <br />
+        <button type="submit">Notiz aktualisieren</button>
+      </form>
+    </div>
+  );
+};
 
 export default NotizBearbeitenFormular;
