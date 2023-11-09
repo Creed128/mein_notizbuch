72c166e80b6bd671cd111f8778ed36763d68d8e4
credotechstarter <146931985+credotechstarter@users.noreply.github.com>
2023-11-09 21:06:23 +0100

    double

--- src/Komponenten/NotizFormular/NeueNotizFormular.js
+++ src/Komponenten/NotizFormular/NeueNotizFormular.js
@@ -13,49 +13,28 @@ const NeueNotizFormular = ({ hinzufuegenNotiz }) => {
     setInhalt(e.target.value);
   };
 
-  const handleIsPublicChange = (e) => {
-    setIsPublic(e.target.value === 'public');
-  };
-
   const handleNeueNotiz = () => {
     if (titel && inhalt) {
       const erstellungsdatum = new Date().toLocaleString();
-      hinzufuegenNotiz({
-        id: Date.now(),
-        title: titel,
-        content: inhalt,
-        isPublic: isPublic,
-        erstellungsdatum,
-      });
+      hinzufuegenNotiz({ id: Date.now(), title: titel, content: inhalt, isPublic: isPublic, erstellungsdatum });
       setTitel('');
       setInhalt('');
+      setIsPublic(isPublic);
     }
   };
 
   return (
     <div className='new-note'>
       <h2>Neue Notiz erstellen</h2>
-      <label className='title' htmlFor="title-input">Titel:</label>
+      <label className='title' for="title-input">Titel:</label>
       <input className='title-input' type="text" value={titel} onChange={handleTitelChange} />
-      <label className='content' htmlFor="content-input">Inhalt:</label>
+      <label className='content' for="content-input">Inhalt:</label>
       <textarea className='content-input' value={inhalt} onChange={handleInhaltChange} />
       <div className='radios'>
-        <input
-          type='radio'
-          name='private-public'
-          value='private'
-          checked={!isPublic}
-          onChange={handleIsPublicChange}
-        />
-        <label htmlFor="private-public">Privat</label>
-        <input
-          type='radio'
-          name='private-public'
-          value='public'
-          checked={isPublic}
-          onChange={handleIsPublicChange}
-        />
-        <label htmlFor="private-public">Öffentlich</label>
+        <input type='radio' name='private-public' value={!isPublic} />
+        <label for="private-public">Privat</label>
+        <input type='radio' name='private-public' value={isPublic} checked />
+        <label for="private-public">Öffentlich</label>
       </div>
       <button className='create-button' onClick={handleNeueNotiz}>Notiz erstellen</button>
     </div>
