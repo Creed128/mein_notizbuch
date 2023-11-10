1ec6a886dd5b98ba14228443dae81b2f4cc1e3b4
EMSCOOL <eduard.mayer@docc.techstarter.de>
2023-11-10 23:43:00 +0100

    komentar Notiz.js

--- src/Komponenten/Notiz/Notiz.js
+++ src/Komponenten/Notiz/Notiz.js
@@ -1,27 +1,33 @@
 import React from 'react';
 
+// Komponente "Notiz"
 const Notiz = ({ notiz, bearbeiteNotiz, loescheNotiz }) => {
-  const handleBearbeiten = () => {
-    const neuerTitel = prompt('Geben Sie den neuen Titel ein:');
-    const neuerInhalt = prompt('Geben Sie den neuen Inhalt ein:');
+  // Funktion zum Bearbeiten der Notiz
+  const handleBearbeiten = () => {
+    // Dialog zur Eingabe des neuen Titels
+    const neuerTitel = prompt('Geben Sie den neuen Titel ein:');
+    // Dialog zur Eingabe des neuen Inhalts
+    const neuerInhalt = prompt('Geben Sie den neuen Inhalt ein:');
 
-    if (neuerTitel !== null && neuerInhalt !== null) {
-      bearbeiteNotiz(notiz.id, { title: neuerTitel, content: neuerInhalt });
-    }
-  };
+    // Überprüfen, ob Titel und Inhalt nicht null sind
+    if (neuerTitel !== null && neuerInhalt !== null) {
+      // Aufruf der bearbeiteNotiz-Funktion mit der Notiz-ID und den neuen Daten
+      bearbeiteNotiz(notiz.id, { title: neuerTitel, content: neuerInhalt });
+    }
+  };
 
-  return (
-    <div className="notiz-element">
-      <h3>{notiz.title}</h3>
-      <p>{notiz.content}</p>
-      <p>{`Erstellt am: ${notiz.erstellungsdatum}`}</p>
-      <button className="bearbeiten-button" onClick={handleBearbeiten}>
-        Bearbeiten
-      </button>
-      <button onClick={() => loescheNotiz(notiz.id)}>Löschen</button>
-    </div>
-  );
+  return (
+    <div className="notiz-element">
+      <h3>{notiz.title}</h3>
+      <p>{notiz.content}</p>
+      <p>{`Erstellt am: ${notiz.erstellungsdatum}`}</p>
+      <button className="bearbeiten-button" onClick={handleBearbeiten}>
+        Bearbeiten
+      </button>
+      <button onClick={() => loescheNotiz(notiz.id)}>Löschen</button>
+    </div>
+  );
 };
 
+// Export der Notiz-Komponente
 export default Notiz;
-
