cbba18fd71120d9165cd1fddb74c9a495d96e201
EMSCOOL <eduard.mayer@docc.techstarter.de>
2023-11-17 22:15:37 +0100

    test umbenannt verschoben

--- src/Hilfsmittel/mocha.test.js
+++ src/Hilfsmittel/mocha.test.js
@@ -2,35 +2,37 @@ require("chromedriver");
 const assert = require('assert');
 const { Builder, By } = require('selenium-webdriver');
 
-describe('ToDo app', async function () {
-    it('ToDo hinzufügen', async function () {
+describe('Notizbuch app', async function () {
+    it('Notiz hinzufügen', async function () {
         let driver = await new Builder().forBrowser('chrome').build();
 
         await driver.get('http://localhost:3000/');
 
-        await driver.findElement(By.className("radios")).sendKeys("Automatisiertes Todo");
-        await driver.findElement(By.className("radios")).click();
+        await driver.findElement(By.id("title-input")).sendKeys("Automatisierte Notiz");
+        await driver.findElement(By.id("Notiz-erstellen")).click();
 
-        let ul = await driver.findElement(By.className("theList"));
-        let listItems = await ul.findElements(By.css("li"));
+        let ul = await driver.findElement(By.className("create-button"));
+        let listItems = await ul.findElements(By.css("li")); // Änderung hier: Verwende By.css statt By.src
 
         assert.equal(listItems.length, 1);
 
         await driver.quit();
     });
 
-    it('2 ToDos hinzufügen', async function () {
+    it('2 Notiz hinzufügen', async function () {
         let driver = await new Builder().forBrowser('chrome').build();
 
         await driver.get('http://localhost:3000/');
 
-        await driver.findElement(By.id("root")).sendKeys("Automatisiertes Todo");
-        await driver.findElement(By.id("root")).click();
+        await driver.findElement(By.id("title-input")).sendKeys("Automatisierte Notiz");
+        await driver.findElement(By.id("Notiz-erstellen")).click();
 
-        await driver.findElement(By.id("root")).sendKeys("Automatisiertes Todo 2");
-        await driver.findElement(By.id("root")).click();
+        await driver.findElement(By.id("title-input")).clear(); // Korrekte Eingabe löschen
 
-        let ul = await driver.findElement(By.className("theList"));
+        await driver.findElement(By.id("title-input")).sendKeys("Automatisierte Notiz 2");
+        await driver.findElement(By.id("Notiz-erstellen")).click();
+
+        let ul = await driver.findElement(By.className("create-button"));
         let listItems = await ul.findElements(By.css("li"));
 
         assert.equal(listItems.length, 2);
