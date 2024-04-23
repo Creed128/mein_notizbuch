a11fa58d7cf2597e3a0d360a2de9ca42d7f4c9eb
Credo Ngoukeng <credongoukeng@gmail.com>
2024-04-23 20:18:51 +0200

    update notidetails.js whit bootstrap

--- src/NotizDetail/NotizDetail.js
+++ src/NotizDetail/NotizDetail.js
@@ -1,14 +1,17 @@
-// src/Komponenten/NotizDetail/NotizDetail.js
 import React from 'react';
 import './NotizDetail.css';
 
-
 class NotizDetail extends React.Component {
   render() {
+    const { notiz } = this.props; // Assurez-vous de passer 'notiz' comme propriété à ce composant
+
     return (
-      <div>
-        <h2>Notiz Detail</h2>
-        {/* Fügen Sie hier den Inhalt der Notizendetails hinzu */}
+      <div className="card">
+        <div className="card-body">
+          <h2 className="card-title">{notiz ? notiz.title : 'Laden...'}</h2>
+          <p className="card-text">{notiz ? notiz.content : 'Keine Details verfügbar.'}</p>
+          <p className="card-text"><small className="text-muted">Erstellt am: {notiz ? notiz.erstellungsdatum : '-'}</small></p>
+        </div>
       </div>
     );
   }
