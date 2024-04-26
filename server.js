8e6d79ba9ee15d83f6690895ae0f62b9a065b497
Eduard_Mayer_IT <eduard.mayer.it@gmail.com>
2024-04-26 11:34:35 +0200

    com

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Successfully connected to mongodb'))
+    .catch(error => console.error(error));
+
+app.get('/', (req, res) => {
+    res.send('Hello Worlddd!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
