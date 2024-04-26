e1ade7e8112a4e56b2753e560b68c5dfbb32a85c
KonstantinTechstarter <konstantin_kirsch@hotmail.com>
2024-04-26 10:19:34 +0200

    update

--- src/models/notizModel.js
+++
@@ -1,9 +0,0 @@
-class NotizModel {
-    constructor(id, description, completed) {
-        this.id = id;
-        this.description = description;
-        this.completed = completed;
-      }
-}
-
-module.exports = NotizModel 
